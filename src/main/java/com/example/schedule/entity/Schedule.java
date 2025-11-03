a7225bfa974d3e168aade29c4ce8e37f6eabd2f9
thk7964 <yjs7894@gmail.com>
2025-11-03 11:34:34 +0900

    feat : 패키지 구성 및 엔티티생성 패키지를 구성했고 일정 엔티티 생성 및 기본 필드추가

---
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -0,0 +1,35 @@
+package com.example.schedule.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Table(name = "schedule")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30, nullable = false)
+    private String title;
+    @Column(length = 200, nullable = false)
+    private String content;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 30, nullable = false)
+    private String password;
+
+    public Schedule(String title, String content,String name, String password){
+        this.title=title;
+        this.content=content;
+        this.name=name;
+        this.password=password;
+    }
+
+    public void update(String title, String content){
+        this.title=title;
+        this.content=content;
+    }
+}
