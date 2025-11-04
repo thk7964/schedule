8a1ce17ba75aef374e5d23c4ed62865ce5faf5a2
thk7964 <yjs7894@gmail.com>
2025-11-04 16:47:17 +0900

    ✨ feat: 댓글을 작성하는 기능 구현
    CommentController에 일정에 댓글을 추가 할 수 있는 기능을 구현

---
+++ src/main/java/com/example/challengeschedule/controller/CommentController.java
@@ -0,0 +1,24 @@
+package com.example.challengeschedule.controller;
+
+import com.example.challengeschedule.dto.CommentRequest;
+import com.example.challengeschedule.dto.CommentResponse;
+import com.example.challengeschedule.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("/schedules/{scheduleId}/comments")
+    public ResponseEntity<CommentResponse> createComment(@PathVariable Long scheduleId, @RequestBody CommentRequest commentRequest) {
+        CommentResponse commentResult = commentService.commentSave(scheduleId, commentRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentResult);
+    }
+}
